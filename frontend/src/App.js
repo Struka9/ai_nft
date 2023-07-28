adcc0ec22132a91b64117cd45affd5479dd6ba3f
Oscar <o_rene89@protonmail.com>
2023-07-27 22:19:46 -0600

    Add first implementation of the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import noImage from "./img/no-image.png";
 import './App.css';
 
+const API_ENDPOINT = "http://127.0.0.1:3001"; //process.env.API_ENDPOINT;
+
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [negativePrompt, setNegativePrompt] = useState("");
+  const [generated, setGenerated] = useState({});
+
+  const handleGenerate = async () => {
+    const body = {
+      prompt,
+      negative_propmpt: negativePrompt
+    }
+
+    const result = await fetch(`${API_ENDPOINT}/images/generate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+
+    if (result.status === 200) {
+      const { output, id } = await result.json();
+      setGenerated({ image: output[0], id });
+    }
+  }
+
+  const handleMint = async () => {
+    // Pin the image
+    // send the tx
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <input placeholder="Enter your prompt" onChange={(e) => setPrompt(e.target.value)} />
+          <input placeholder="What things you don't want to see?" onChange={(e) => setNegativePrompt(e.target.value)} />
+        </div>
+        <button onClick={handleGenerate}>Generate</button>
+      </div>
+      <img width="512" height="512" alt="" src={!generated ? noImage : generated.image} />
+      {generated.image && <button onClick={handleMint}>Mint NFT</button>}
     </div>
   );
 }
