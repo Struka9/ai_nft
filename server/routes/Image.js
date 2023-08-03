153a2cedfd0238d39af6aba7dbeeae1a6762d57a
Oscar <o_rene89@protonmail.com>
2023-08-03 11:06:54 -0600

    Sign token's metadata hash, respond with hash and signature in POS /pin/:id endpoint

--- server/routes/Image.js
+++ server/routes/Image.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import dotenv from "dotenv";
 import { Readable } from "stream";
 import FormData from "form-data";
-import fs from "fs";
+import crypto from "crypto";
+import { ethers } from "ethers";
 
 dotenv.config();
 
@@ -47,23 +48,69 @@ router.post("/pin/:id", async (req, res) => {
 
                 // Pin file to Pinata
                 const formData = new FormData();
-                formData.append("file", readStream, { filepath: "random.png" });
+                const hash = crypto.createHash("sha256");
+                hash.update(id.toString());
+                const hashDigest = hash.digest("hex");
+                formData.append("file", readStream, { filepath: `${hashDigest}.png` });
 
                 const options = JSON.stringify({
                     cidVersion: 0,
                 })
                 formData.append('pinataOptions', options);
 
-                const pinResponse = await axios.post(process.env.PINATA_PIN_FILE_ENDPOINT, formData, {
+                const pinImageResponse = await axios.post(process.env.PINATA_PIN_FILE_ENDPOINT, formData, {
                     maxBodyLength: "Infinity",
                     headers: {
                         'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
                         Authorization: `Bearer ${process.env.PINATA_JWT}`
-                    }
+                    },
+                    responseType: 'json'
                 });
 
-                if (pinResponse.status == 200) {
-                    return res.json(pinResponse.data);
+                if (pinImageResponse.status == 200) {
+                    console.log(`pin image = ${pinImageResponse.data.IpfsHash}`);
+
+                    const nftBody = {
+                        name: "CoolNFT",
+                        description: "A collection of NFTs created using AI.",
+                        image: `ipfs://${pinImageResponse.data.IpfsHash}`,
+                        properties: {
+                            prompt: "",
+                            negative_prompt: ""
+                        }
+                    }
+
+                    const pinJsonResponse = await axios.post(process.env.PINATA_PIN_JSON_ENDPOINT, nftBody, {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Authorization: `Bearer ${process.env.PINATA_JWT}`
+                        },
+                        responseType: 'json'
+                    });
+
+                    if (pinJsonResponse.status == 200) {
+                        // Sign the IPFS hash returned here
+                        const signer = new ethers.Wallet("0x" + process.env.SIGNER_PK);
+                        let message = ethers.solidityPacked(["string"], [pinJsonResponse.data.IpfsHash]);
+                        console.log(`message before hash => ${message}`);
+                        message = ethers.keccak256(ethers.getBytes(message));
+                        console.log(`message after hash => ${message}`);
+                        const arrayify = ethers.getBytes(message);
+                        console.log(`as array => ${arrayify}`);
+                        const signature = await signer.signMessage(arrayify);
+                        console.log(`signature => ${signature}`);
+                        // Send back the hash + signature
+                        res.json({
+                            IpfsHash: pinJsonResponse.data.IpfsHash,
+                            signature
+                        });
+                    } else {
+                        console.error("failed pining JSON");
+                        return res.status(500);
+                    }
+                } else {
+                    console.error("failed pining image");
+                    return res.status(500);
                 }
             } else {
                 console.error("fetch image request failed");
