497d00f8e777d1f5438a55e0e6785e853232a214
Oscar <o_rene89@protonmail.com>
2023-08-03 20:37:55 -0600

    Add test to the DeployCoolNFT script Refactor HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -8,6 +8,9 @@ contract HelperConfig {
 
     NetworkConfig public s_activeNetworkConfig;
 
+    address constant ANVIL_DEFAULT_ADDRESS =
+        address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+
     constructor() {
         if (block.chainid == 31337) {
             s_activeNetworkConfig = getOrCreateAnvilConfig();
@@ -25,12 +28,7 @@ contract HelperConfig {
         pure
         returns (NetworkConfig memory)
     {
-        return
-            NetworkConfig({
-                authorizedSigner: address(
-                    0x1C6C09E3dfd587DC3D9087254412c6a811759345
-                )
-            });
+        return NetworkConfig({authorizedSigner: ANVIL_DEFAULT_ADDRESS});
     }
 
     function getSepoliaConfig() internal pure returns (NetworkConfig memory) {
---
+++ test/DeployCoolNFTTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {CoolNFT} from "../src/CoolNFT.sol";
+import {DeployCoolNFT} from "../script/DeployCoolNFT.s.sol";
+
+contract DeployCoolNFTTest is Test {
+    // authorized signer's address: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+    // authorized signer's PK: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+    address BOB = makeAddr("bob");
+
+    string TOKEN_URI = "ipfs://QmdHT3ai8PfUsyVAftbFHagCEUYcVRgi4TCyGUJ6rmAVcQ";
+    string TOKEN_SAMPLE_HASH = "QmdHT3ai8PfUsyVAftbFHagCEUYcVRgi4TCyGUJ6rmAVcQ";
+    bytes TOKEN_SAMPLE_SIGNATURE =
+        abi.encodePacked(
+            hex"eacd997e039a226b1bdf14fea6ec30301be1c12593e2d74066d60c2458e7d8bf45b976670cfb356bf227338e7e764536988229c926e6c38abf7a83231ec428b21b"
+        );
+    bytes TOKEN_INVALID_SIGNATURE =
+        abi.encodePacked(
+            hex"ffff997e039a226b1bdf14fea6ec30301be1c12593e2d74066d60c2458e7d8bf45b976670cfb356bf227338e7e764536988229c926e6c38abf7a83231ec428b21b"
+        );
+
+    function testDeploy() public {
+        DeployCoolNFT deployer = new DeployCoolNFT();
+        CoolNFT instance = deployer.run();
+        vm.startPrank(BOB);
+        instance.mint(TOKEN_SAMPLE_HASH, TOKEN_SAMPLE_SIGNATURE);
+        vm.stopPrank();
+
+        assertEq(instance.tokenURI(0), TOKEN_URI);
+    }
+}
