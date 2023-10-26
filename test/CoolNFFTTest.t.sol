aef06d6b92dd4c6da48bffd4578d3083e20520fc
Oscar <o_rene89@protonmail.com>
2023-10-26 14:57:32 -0400

    Finalize contract

--- test/CoolNFFTTest.t.sol
+++ test/CoolNFFTTest.t.sol
@@ -13,14 +13,12 @@ contract CoolNFTTest is Test {
 
     string TOKEN_URI = "ipfs://QmdHT3ai8PfUsyVAftbFHagCEUYcVRgi4TCyGUJ6rmAVcQ";
     string TOKEN_SAMPLE_HASH = "QmdHT3ai8PfUsyVAftbFHagCEUYcVRgi4TCyGUJ6rmAVcQ";
-    bytes TOKEN_SAMPLE_SIGNATURE =
-        abi.encodePacked(
-            hex"eacd997e039a226b1bdf14fea6ec30301be1c12593e2d74066d60c2458e7d8bf45b976670cfb356bf227338e7e764536988229c926e6c38abf7a83231ec428b21b"
-        );
-    bytes TOKEN_INVALID_SIGNATURE =
-        abi.encodePacked(
-            hex"ffff997e039a226b1bdf14fea6ec30301be1c12593e2d74066d60c2458e7d8bf45b976670cfb356bf227338e7e764536988229c926e6c38abf7a83231ec428b21b"
-        );
+    bytes TOKEN_SAMPLE_SIGNATURE = abi.encodePacked(
+        hex"eacd997e039a226b1bdf14fea6ec30301be1c12593e2d74066d60c2458e7d8bf45b976670cfb356bf227338e7e764536988229c926e6c38abf7a83231ec428b21b"
+    );
+    bytes TOKEN_INVALID_SIGNATURE = abi.encodePacked(
+        hex"ffff997e039a226b1bdf14fea6ec30301be1c12593e2d74066d60c2458e7d8bf45b976670cfb356bf227338e7e764536988229c926e6c38abf7a83231ec428b21b"
+    );
 
     function setUp() public {
         instance = new CoolNFT(
